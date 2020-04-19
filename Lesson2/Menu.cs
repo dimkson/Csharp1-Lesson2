42ad69cd21e36e981e1194cef80eac66cfcbf6e2
Дмитрий Быков <dimkson@yahoo.com>
2020-04-19 18:44:32 +0300

    Добавьте файлы проекта.

---
+++ Lesson2/Menu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace MenuClass
+{
+    class Menu
+    {
+        public delegate void delMenu();
+        private delMenu[] delMenus;
+
+        public Menu(delMenu[] delegates)
+        {
+            delMenus = delegates;
+        }
+
+        public void ChooseMenu()
+        {
+            int num;
+            do
+            {
+                num = 1 ;
+                Console.WriteLine($"Для выбора задания введите номер задания от 1 до {delMenus.Length}");
+                foreach (delMenu del in delMenus)
+                {
+                    Console.WriteLine(num++ + " - " + del.Method.Name);
+                }
+                Console.WriteLine("Для выхода из меню нажмите 10");
+                try
+                {
+                    Int32.TryParse(Console.ReadLine(), out num);
+                    delMenus[num - 1]();
+                    Console.Clear();
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            } while (num != 10);
+        }
+    }
+
+    static class FastConsole
+    {
+        public static void Input(string str, out int num)
+        {
+            Console.WriteLine(str);
+            Int32.TryParse(Console.ReadLine(), out num);
+        }
+        public static void Input(string str, out double num)
+        {
+            Console.WriteLine(str);
+            double.TryParse(Console.ReadLine(), out num);
+        }
+        public static string Input(string str)
+        {
+            Console.WriteLine(str);
+            return Console.ReadLine();
+        }
+        public static void Pause()
+        {
+            Console.ReadLine();
+        }
+        public static void Print(string str, int x, int y)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,152 @@
+﻿using MenuClass;
+using System;
+using FC = MenuClass.FastConsole;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.delMenu[] delMenus = new Menu.delMenu[] { Task1, Task2, Task3, Task4, Task5, Task6, Task7 };
+            Menu menu = new Menu(delMenus);
+            menu.ChooseMenu();
+        }
+
+        #region Задание1
+        static void Task1()
+        {
+            FC.Input("Введите первое число", out int first);
+            FC.Input("Введите второе число", out int second);
+            FC.Input("Введите третье число", out int third);
+
+            if (second < first) first = second;
+            if (third < first) first = third;
+            Console.WriteLine("Минимальное значение: " + first);
+            FC.Pause();
+        }
+        #endregion
+        #region Задание2
+        static void Task2()
+        {
+            FC.Input("Введите число", out int x);
+            int s = 0;
+            if (x != 0)
+            {
+                while (x != 0)
+                {
+                    x = x / 10;
+                    s++;
+                }
+                Console.WriteLine("Количество цифр: " + s);
+            }
+            else
+                Console.WriteLine("Неверный ввод!");
+            FC.Pause();
+        }
+        #endregion
+        #region Задание3
+        static void Task3()
+        {
+            int x, s = 0;
+            do
+            {
+                FC.Input("Введите число или 0 для выхода", out x);
+                if (x > 0 && x % 2 != 0) s += x;
+            } while (x != 0);
+            Console.WriteLine("Сумма введенных нечетных положительных чисел равна " + s);
+            FC.Pause();
+        }
+        #endregion
+        #region Задание4
+        static void Task4()
+        {
+            int x = 0;
+            do
+            {
+                string login = FC.Input("Введите логин");
+                string password = FC.Input("Введите пароль");
+                if (IsValid(login, password))
+                {
+                    Console.WriteLine("Успех!");
+                    FC.Pause();
+                    return;
+                }
+                Console.WriteLine("Неверный логин или пароль");
+                x++;
+            } while (x != 3);
+            Console.WriteLine("Access Denied");
+            FC.Pause();
+        }
+        static bool IsValid(string login, string password)
+        {
+            return (login == "root" && password == "GeekBrains");
+        }
+        #endregion
+        #region Задание5
+        static void Task5()
+        {
+            FC.Input("Введите рост в метрах", out double h);
+            FC.Input("Введите вес", out double m);
+            double I = m / (h * h);
+            if (I < 18.5)
+            {
+                Console.WriteLine("Ваш вес ниже нормы!");
+                Console.WriteLine("Необходимо набрать " + (18.5 * h * h - m) + " кг.");
+            }
+            else if (I >= 25)
+            {
+                Console.WriteLine("Ваш вес выше нормы!");
+                Console.WriteLine("Необходимо сбросить " + (m - 25 * h * h) + " кг.");
+            }
+            else
+                Console.WriteLine("Ваш вес в норме!");
+            FC.Pause();
+        }
+        #endregion
+        #region Задание6
+        static void Task6()
+        {
+            DateTime start = DateTime.Now;
+            int s, x, good = 0;
+
+            for(int i = 1; i < 100000000; i++)
+            {
+                x = i;
+                s = 0;
+                while (x != 0)
+                {
+                    s += x % 10;
+                    x /= 10;
+                }
+                if (i % s == 0) good++;
+            }
+            Console.WriteLine("Количество хороших чисел равно " + good);
+            Console.WriteLine("Время подсчета " + (DateTime.Now - start));
+            FC.Pause();
+        }
+        #endregion
+        #region Задание7
+        static void Task7()
+        {
+            FC.Input("Введите a", out int a);
+            FC.Input("Введите b", out int b);
+            Console.Write(a);
+            RecurPrint(a + 1, b);
+            Console.WriteLine();
+            Console.WriteLine(RecurSum(a, b));
+            FC.Pause();
+        }
+        static void RecurPrint(int a, int b)
+        {
+            Console.Write(", " + a);
+            if (a < b) RecurPrint(++a, b);
+        }
+        static int RecurSum(int a, int b)
+        {
+            if (a > b) return 0;
+            return a + RecurSum(++a, b);
+        }
+        #endregion
+    }
+}
